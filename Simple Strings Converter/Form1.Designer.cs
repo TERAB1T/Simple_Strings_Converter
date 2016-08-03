b4cb30be9546f75c4d074f92704ac555517d4d42
TERAB1T <akiselyov@ukr.net>
2016-08-03 17:26:47 +0300

    Initial

---
+++ Simple Strings Converter/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Simple_Strings_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(447, 300);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(13, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(447, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Old -> New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(13, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(447, 49);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "New -> Old";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 439);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Form1";
+            this.Text = "Simple Strings Converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Simple Strings Converter/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Simple_Strings_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ToRus(string input)
+        {
+            input = input.Replace('А', 'A');
+            input = input.Replace('Б', 'À');
+            input = input.Replace('В', 'Á');
+            input = input.Replace('Г', 'Â');
+            input = input.Replace('Д', 'Ä');
+            input = input.Replace('Е', 'E');
+            input = input.Replace('Ё', 'E');
+            input = input.Replace('Ж', 'Æ');
+            input = input.Replace('З', 'Ç');
+            input = input.Replace('И', 'È');
+            input = input.Replace('Й', 'É');
+            input = input.Replace('К', 'Ê');
+            input = input.Replace('Л', 'Ì');
+            input = input.Replace('М', 'Í');
+            input = input.Replace('Н', 'Î');
+            input = input.Replace('О', 'O');
+            input = input.Replace('П', 'Ï');
+            input = input.Replace('Р', 'P');
+            input = input.Replace('С', 'C');
+            input = input.Replace('Т', 'Ò');
+            input = input.Replace('У', 'Ó');
+            input = input.Replace('Ф', 'Ñ');
+            input = input.Replace('Х', 'X');
+            input = input.Replace('Ц', 'Œ');
+            input = input.Replace('Ч', 'Ù');
+            input = input.Replace('Ш', 'Ú');
+            input = input.Replace('Щ', 'Û');
+            input = input.Replace('Ъ', 'Å');
+            input = input.Replace('Ы', 'Ÿ');
+            input = input.Replace('Ь', 'Ö');
+            input = input.Replace('Э', 'Ë');
+            input = input.Replace('Ю', 'Ô');
+            input = input.Replace('Я', 'Ü');
+            input = input.Replace('а', 'a');
+            input = input.Replace('б', 'à');
+            input = input.Replace('в', 'á');
+            input = input.Replace('г', 'â');
+            input = input.Replace('д', 'ä');
+            input = input.Replace('е', 'e');
+            input = input.Replace('ё', 'e');
+            input = input.Replace('ж', 'æ');
+            input = input.Replace('з', 'ç');
+            input = input.Replace('и', 'è');
+            input = input.Replace('й', 'é');
+            input = input.Replace('к', 'ê');
+            input = input.Replace('л', 'ì');
+            input = input.Replace('м', 'í');
+            input = input.Replace('н', 'î');
+            input = input.Replace('о', 'o');
+            input = input.Replace('п', 'ï');
+            input = input.Replace('р', 'p');
+            input = input.Replace('с', 'c');
+            input = input.Replace('т', 'ò');
+            input = input.Replace('у', 'ó');
+            input = input.Replace('ф', 'ñ');
+            input = input.Replace('х', 'x');
+            input = input.Replace('ц', 'œ');
+            input = input.Replace('ч', 'ù');
+            input = input.Replace('ш', 'ú');
+            input = input.Replace('щ', 'û');
+            input = input.Replace('ъ', 'å');
+            input = input.Replace('ы', 'ÿ');
+            input = input.Replace('ь', 'ö');
+            input = input.Replace('э', 'ë');
+            input = input.Replace('ю', 'ô');
+            input = input.Replace('я', 'ü');
+            return input;
+        }
+
+        public string ToRusBack(string input)
+        {
+            input = input.Replace('A', 'А');
+            input = input.Replace('À', 'Б');
+            input = input.Replace('Á', 'В');
+            input = input.Replace('Â', 'Г');
+            input = input.Replace('Ä', 'Д');
+            input = input.Replace('E', 'Е');
+            input = input.Replace('E', 'Ё');
+            input = input.Replace('Æ', 'Ж');
+            input = input.Replace('Ç', 'З');
+            input = input.Replace('È', 'И');
+            input = input.Replace('É', 'Й');
+            input = input.Replace('Ê', 'К');
+            input = input.Replace('Ì', 'Л');
+            input = input.Replace('Í', 'М');
+            input = input.Replace('Î', 'Н');
+            input = input.Replace('O', 'О');
+            input = input.Replace('Ï', 'П');
+            input = input.Replace('P', 'Р');
+            input = input.Replace('C', 'С');
+            input = input.Replace('Ò', 'Т');
+            input = input.Replace('Ó', 'У');
+            input = input.Replace('Ñ', 'Ф');
+            input = input.Replace('²', 'Ф'); //старое
+            input = input.Replace('X', 'Х');
+            input = input.Replace('Œ', 'Ц');
+            input = input.Replace('Ù', 'Ч');
+            input = input.Replace('Ú', 'Ш');
+            input = input.Replace('Û', 'Щ');
+            input = input.Replace('µ', 'Ъ'); //старое
+            input = input.Replace('Å', 'Ъ');
+            input = input.Replace('Ÿ', 'Ы');
+            input = input.Replace('Ö', 'Ь');
+            input = input.Replace('Ë', 'Э');
+            input = input.Replace('Ô', 'Ю');
+            input = input.Replace('Ü', 'Я');
+            input = input.Replace('a', 'а');
+            input = input.Replace('à', 'б');
+            input = input.Replace('á', 'в');
+            input = input.Replace('â', 'г');
+            input = input.Replace('ä', 'д');
+            input = input.Replace('e', 'е');
+            input = input.Replace('e', 'ё');
+            input = input.Replace('æ', 'ж');
+            input = input.Replace('ç', 'з');
+            input = input.Replace('è', 'и');
+            input = input.Replace('é', 'й');
+            input = input.Replace('ê', 'к');
+            input = input.Replace('ì', 'л');
+            input = input.Replace('í', 'м');
+            input = input.Replace('î', 'н');
+            input = input.Replace('o', 'о');
+            input = input.Replace('ï', 'п');
+            input = input.Replace('p', 'р');
+            input = input.Replace('c', 'с');
+            input = input.Replace('ò', 'т');
+            input = input.Replace('ó', 'у');
+            input = input.Replace('ñ', 'ф');
+            input = input.Replace('³', 'ф'); //старое
+            input = input.Replace('x', 'х');
+            input = input.Replace('œ', 'ц');
+            input = input.Replace('ù', 'ч');
+            input = input.Replace('ú', 'ш');
+            input = input.Replace('û', 'щ');
+            input = input.Replace('°', 'ъ'); //старое
+            input = input.Replace('å', 'ъ');
+            input = input.Replace('ÿ', 'ы');
+            input = input.Replace('ö', 'ь');
+            input = input.Replace('ë', 'э');
+            input = input.Replace('ô', 'ю');
+            input = input.Replace('ü', 'я');
+            return input;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ToRusBack(richTextBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ToRus(richTextBox1.Text);
+        }
+    }
+}
